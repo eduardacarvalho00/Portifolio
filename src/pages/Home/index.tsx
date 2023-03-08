e2b68956932e271de3a616b9098da3ba3ef3914a
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-03-08 11:44:48 -0300

    indentation refactoring

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -37,10 +37,10 @@ export default function Home() {
         <Heading as="h1" textAlign={'center'} fontWeight="medium" pb={['30', '100', '145']} fontSize={['22', '26']}>Who <Text as="span" color="orange.500"> I </Text> am?</Heading>
 
         <Text textAlign={'justify'} lineHeight="1.7" fontWeight="regular" p={[3, 0, 0, 0]} fontSize={['16', '18']}>
-        My name is <Text as="span" color="orange.500">Eduarda Carvalho</Text>, I'm 18 years old and I'm in the third quarter of Systems Analysis and Development at Anhembi Morumbi.
-        <br />
-        I've always been very close to technology, especially video games, this made me fall in love with this area so at the end of 2021 I decided to become fullstack,
-        I actually decided to venture into FrontEnd :)
+          My name is <Text as="span" color="orange.500">Eduarda Carvalho</Text>, I'm 18 years old and I'm in the third quarter of Systems Analysis and Development at Anhembi Morumbi.
+          <br />
+          I've always been very close to technology, especially video games, this made me fall in love with this area so at the end of 2021 I decided to become fullstack,
+          I actually decided to venture into FrontEnd :)
         </Text>
 
       </Box>
