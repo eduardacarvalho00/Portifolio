4d5f0752c9aa278eee512faa87a4df57fe2405e8
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-03-08 21:59:06 -0300

    feat: add json with projects

--- src/components/CardProjects.tsx
+++ src/components/CardProjects.tsx
@@ -1,6 +1,7 @@
 import {
-  Flex, Image, Heading, Link, Button, Text, Box, useColorModeValue, 
+  Flex, Image, Heading, Button, Text, Box, useColorModeValue, Link, 
 } from '@chakra-ui/react';
+import { MotionBox } from '../styles/animation';
 
 interface CardProjectProps {
   src: string;
@@ -15,69 +16,70 @@ export function CardsProjects({
   const borderColor = useColorModeValue('gray.600', 'gray.500');
   const bgCard = useColorModeValue('linear(to-l, blackAlpha.400, whiteAlpha.100)', 'linear(to-l, whiteAlpha.200, whiteAlpha.100)');
   return (
-       <Flex
-         flexDirection={'column'}
-         maxW={350}
-         h={370}
-         border="1px solid"
-         borderColor={borderColor}
-         bgGradient={bgCard}
-         p={'3'}
-         borderTopStartRadius={20}
-         borderBottomEndRadius={20}
-         mr={[0, 6]}
-         mb={4}
+    <MotionBox whileHover={{ scale: 1.05 }}> 
+      <Flex
+        flexDirection="column"
+        maxW={450}
+        h={400}
+        border="1px solid"
+        borderColor={borderColor}
+        bgGradient={bgCard}
+        p="3"
+        borderRadius={6}
+        mr={[0, 6]}
+        mb={4}
+        cursor="default"
       >
         <Image
           src={src}
           maxW={'100%'}
-          minH={125}
+          minH="150px"
           borderRadius={5}
-          border="1px solid"
-          borderColor={borderColor}
         />
         <Heading
-          fontWeight={'medium'}
-          fontSize={'20'}
-          textAlign={'center'}
+          fontWeight="medium"
+          fontSize={26}
+          textAlign="center"
           p={4}
-          color={'orange.500'}
+          color="orange.500"
         >
           {title}
         </Heading>
 
         <Box
-          overflow={'auto'}
-          h={'180'}
-          maxW={'100%'}
+          overflow="auto"
+          h="180"
+          maxW="100%"
           css={{
             '&::-webkit-scrollbar': {
               width: '4px',
             },
             '&::-webkit-scrollbar-thumb': {
-              background: 'gray',
+              background: '#FF4D00',
               borderRadius: '24px',
             },
           }}
         >
-          <Text textAlign={'justify'}>{text}</Text>
+          <Text textAlign="justify">{text}</Text>
         </Box>
         
         <Link href={href} target="_blank">
           <Button
-            bg={'orange.500'}
-            fontWeight={'regular'}
+            bg="orange.500"
             mt={8}
-            size={'sm'}
-            w={'100%'}
+            size="sm"
+            w="100%"
             color="white"
             _hover={{
-              bg: 'orange.400',
+              bg: 'orange.800',
             }}
           >
-            GO
+            <Text fontWeight="light">
+              GO
+            </Text>
           </Button>
         </Link>
       </Flex>
+    </MotionBox>   
   );
 }
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -2,12 +2,11 @@ import {
   Flex, Heading,
 } from '@chakra-ui/react';
 import { CardsProjects } from '../../components/CardProjects';
-import aluroniProjeto from '../../assets/aluroni_projeto.jpg';
-import skyBlinded from '../../assets/skyeBlinded.jpg';
-import dtMoney from '../../assets/dtMoney.jpg';
-import dashgo from '../../assets/dashgo.jpg';
+import projects from '../../data/projects.json';
 
-export default function Projects() {
+export default function Projects() { 
+  console.log(projects);
+  
   return (
     <Flex
       id="projects"
@@ -16,13 +15,12 @@ export default function Projects() {
       maxW={['300', '550', '700', '1191']}
       mx="auto"
       mb={50}
-      flexDirection={'column'}
+      flexDirection="column"
       align="center"
     >
-      
       <Heading
         as="h1"
-        textAlign={'center'}
+        textAlign="center"
         fontWeight="medium"
         pb={['10', '100']}
         fontSize={['22', '36']}
@@ -30,34 +28,15 @@ export default function Projects() {
       >
         Projects
       </Heading>
-        <Flex 
-          flexDirection={{ base: 'column', md: 'column', lg: 'row' }}
-         
-        >
-          <CardsProjects
-            src={aluroniProjeto}
-            title="Aluroni"
-            text="Restaurant website, made with react.js, sass, router DOM and module.css Search and filter by menu."
-            href="https://react-lidando-com-arquivos-est-ticos.vercel.app"
+        <Flex flexDirection={{ base: 'column', md: 'column', lg: 'row' }}>
+          {projects.map((project) => (
+            <CardsProjects
+              src={project.photo}
+              title={project.title}
+              text={project.description}
+              href={project.linkUrl}
             />
-          <CardsProjects
-            src={dashgo}
-            title="Dashgo"
-            text="Responsive dashboard with chakra UI, Next and typescript"
-            href="https://dashgo-httmkhaiz-eduardacarvalho00.vercel.app"
-          />
-          <CardsProjects
-            src={skyBlinded}
-            title="Sky Blinded"
-            text="Website to consult the weather forecast using api, react.js, styled components and typescript"
-            href="https://sky-blinded.vercel.app"
-          />
-          <CardsProjects
-            src={dtMoney}
-            title="Dt Money"
-            text="Spending Control, Shows Your Transactions and Allows You to Launch a New Transaction | using the MirageJS library, react.js, styled components and typescript"
-            href="https://dtmoney-ignite2.vercel.app"
-          />
+          ))}
         </Flex>
     </Flex>
   );
